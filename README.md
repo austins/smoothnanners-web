5b474d2b172cf35bf806fd16076e6f1e19f00c87
austins <austins@users.noreply.github.com>
2022-05-19 18:10:26 -0700

    Update readme

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 The [SmoothNanners](https://smoothnanners.com) website.
 
 Powered by Next.js.
-
-## Development
-
-Preferably, configure your IDE to run `eslint --fix` on save for file extensions: `{**/*,*}.{js,ts,jsx,tsx}` and prettier on code reformat and save for file extensions: `{**/*,*}.{js,ts,jsx,tsx,json,css,scss}`.
--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 const { withPlugins, optional } = require("next-compose-plugins");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-};
+const nextConfig = { reactStrictMode: true, swcMinify: true };
 
 module.exports = withPlugins(
     [[optional(() => require("@next/bundle-analyzer")({ enabled: process.env.ANALYZE === "true" }))]],
