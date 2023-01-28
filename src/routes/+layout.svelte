965f6f5dc020f91587d9e7b30664c226fe7149fb
austins <austins@users.noreply.github.com>
2023-01-27 16:39:12 -0800

    Add description meta tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
     import "../app.css";
 </script>
 
+<svelte:head>
+    <meta name="description" content="SmoothNanners is a gamer, musician, software developer, and photographer." />
+</svelte:head>
+
 <div
     class="flex min-h-screen items-center text-xl leading-snug text-white drop-shadow-[0_1px_4px_rgba(255,255,255,0.15)] sm:text-2xl"
     data-sveltekit-preload-data="tap"
