c0610ac92ab6a8f53551b69b313c67dd88d47347
austins <austins@users.noreply.github.com>
2023-02-10 02:22:24 -0800

    Fix invalid HTML (nesting button inside anchor)

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let text: string;
+    export let href: string | null = null;
+    export let type: svelteHTML.IntrinsicElements["button"]["type"] = "button";
+
+    $: isLink = !!href?.length;
+</script>
+
+<svelte:element
+    this={isLink ? "a" : "button"}
+    {href}
+    type={!isLink ? type : null}
+    class="rounded-md bg-blue-600 px-3 py-2 hover:bg-blue-700 active:bg-blue-700"
+    class:inline-block={isLink}
+>
+    {text}
+</svelte:element>
--- src/routes/404/+page.svelte
+++ src/routes/404/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
     import Error from "$lib/components/Error.svelte";
+    import Button from "$lib/components/Button.svelte";
 </script>
 
 <Error override={{ status: 404, message: "Page not found." }} />
 
 <div class="mt-4">
-    <a href="/">
-        <button type="button">Go to Home</button>
-    </a>
+    <Button href="/" text="Go to Home" />
 </div>
