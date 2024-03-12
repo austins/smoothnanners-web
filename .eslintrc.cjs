79c4939b37b9eba38e017976cce6ee06094339e5
austins <austins@users.noreply.github.com>
2024-03-12 00:03:34 -0700

    Switch from prettier to ESLint Stylistic

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const stylisticRules = require("@stylistic/eslint-plugin").configs.customize({
+    indent: 4,
+    quotes: "double",
+    semi: true,
+    jsx: true,
+    commaDangle: "never",
+    braceStyle: "1tbs",
+    arrowParens: true
+}).rules;
+
+/** @type { import("eslint").Linter.Config } */
 module.exports = {
     root: true,
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:svelte/recommended", "prettier"],
-    plugins: ["@typescript-eslint"],
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:svelte/recommended"],
+    plugins: ["@typescript-eslint", "@stylistic"],
     parser: "@typescript-eslint/parser",
     parserOptions: { sourceType: "module", ecmaVersion: 2020, extraFileExtensions: [".svelte"] },
     env: { browser: true, es2017: true, node: true },
@@ -12,16 +24,18 @@ module.exports = {
     rules: {
         eqeqeq: "error",
         curly: ["error", "all"],
-        "brace-style": ["error", "1tbs"],
         "object-shorthand": ["error", "always"],
         "require-await": "error",
         "no-return-await": "error",
-        "@typescript-eslint/consistent-type-imports": ["error", { fixStyle: "separate-type-imports" }],
+        "@typescript-eslint/consistent-type-imports": ["error", { fixStyle: "inline-type-imports" }],
         "@typescript-eslint/no-non-null-assertion": "off",
         "@typescript-eslint/no-unused-vars": [
             "error",
             { vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" }
         ],
-        "@typescript-eslint/no-explicit-any": "off"
+        "@typescript-eslint/no-explicit-any": "off",
+        ...stylisticRules,
+        "@stylistic/jsx-one-expression-per-line": "off",
+        "@stylistic/quote-props": ["error", "as-needed"]
     }
 };
--- .prettierrc.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import("prettier").Config} */
-module.exports = {
-    plugins: ["prettier-plugin-svelte"],
-    overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
-    trailingComma: "none",
-    htmlWhitespaceSensitivity: "ignore"
-};
