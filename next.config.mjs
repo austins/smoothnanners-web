f1e23af344b9c05a2e85967d3c5723496ba14918
austins <austins@users.noreply.github.com>
2024-03-22 04:30:45 -0700

    Add comment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const config = {
-    eslint: { ignoreDuringBuilds: true },
+    eslint: {
+        ignoreDuringBuilds: true // next lint doesn't work with our eslint config, so we should run eslint directly.
+    },
     output: "export",
     images: { unoptimized: true }
 };
