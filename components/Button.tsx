eaac9e4d965db95e723e7b36ca57f3e6a0f5966b
austins <austins@users.noreply.github.com>
2024-03-22 12:50:57 -0700

    Clean up Button props

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,21 +1,17 @@
-"use client";
-
-import Link, { type LinkProps } from "next/link";
-import { type ButtonHTMLAttributes, type PropsWithChildren } from "react";
+import Link from "next/link";
+import { type ComponentProps, type PropsWithChildren } from "react";
 import { twJoin } from "tailwind-merge";
 
 const classNames = "rounded-md bg-blue-600 px-3 py-2 hover:bg-blue-700 active:bg-blue-700";
 
 export function Button(
-    props: PropsWithChildren<
-        | ({ as: "button" } & ButtonHTMLAttributes<HTMLButtonElement>)
-        | ({ as: "a" } & LinkProps)>
+    { as, children, ...props }: PropsWithChildren<
+        | ({ as: "button" } & ComponentProps<"button">)
+        | ({ as: "a" } & ComponentProps<typeof Link>)>
 ) {
-    const { as, children, ...restProps } = props;
-
     if (as === "a") {
-        return <Link className={twJoin(classNames, "inline-block")} {...restProps as LinkProps}>{children}</Link>;
+        return <Link className={twJoin(classNames, "inline-block")} {...props as ComponentProps<typeof Link>}>{children}</Link>;
     }
 
-    return <button className={classNames} {...restProps as ButtonHTMLAttributes<HTMLButtonElement>}>{children}</button>;
+    return <button className={classNames} {...props as ComponentProps<"button">}>{children}</button>;
 }
