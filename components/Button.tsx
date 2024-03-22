ae090c25e2800173a61ac3f82149c05e9c3cc2b4
austins <austins@users.noreply.github.com>
2024-03-22 13:16:54 -0700

    Fix button props type

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,11 +4,12 @@ import { twJoin } from "tailwind-merge";
 
 const classNames = "rounded-md bg-blue-600 px-3 py-2 hover:bg-blue-700 active:bg-blue-700";
 
-export function Button(
-    { as, children, ...props }: PropsWithChildren<
-        | ({ as: "button" } & ComponentProps<"button">)
-        | ({ as: "a" } & ComponentProps<typeof Link>)>
-) {
+export function Button<T>({ as, children, ...props }: PropsWithChildren<
+    { as?: T extends "a" | "button" ? T : never }
+    & (T extends "a"
+        ? ComponentProps<typeof Link>
+        : ComponentProps<"button">)
+>) {
     if (as === "a") {
         return <Link className={twJoin(classNames, "inline-block")} {...props as ComponentProps<typeof Link>}>{children}</Link>;
     }
