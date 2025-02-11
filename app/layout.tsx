db96230a71e2731b5fa35f1c497e46da931e3497
austins <austins@users.noreply.github.com>
2025-02-11 14:30:08 -0800

    Update header font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,29 @@
 import "./app.css";
 import type { Metadata } from "next";
 import type { PropsWithChildren } from "react";
+import { Playpen_Sans } from "next/font/google";
+import { twJoin } from "tailwind-merge";
 
 export const metadata: Metadata = {
     title: { template: "%s | SmoothNanners", default: "SmoothNanners" },
     description: "SmoothNanners is a gamer, musician, software developer, and photographer."
 };
 
+const playpenSansFont = Playpen_Sans({
+    subsets: ["latin"],
+    variable: "--font-playpen-sans",
+    display: "swap"
+});
+
 export default function RootLayout(props: PropsWithChildren) {
     return (
         <html lang="en">
-            <body className="font-rounded-sans flex min-h-screen items-center bg-blue-950 bg-gradient-to-b from-blue-950 to-blue-800 text-2xl text-white drop-shadow">
+            <body
+                className={twJoin(
+                    playpenSansFont.variable,
+                    "font-rounded-sans flex min-h-screen items-center bg-blue-950 bg-gradient-to-b from-blue-950 to-blue-800 text-2xl text-white drop-shadow"
+                )}
+            >
                 <div className="container py-4">
                     <header className="pb-3">
                         <h1 className="font-handwritten text-5xl font-bold">SmoothNanners</h1>
