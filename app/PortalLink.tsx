91d7fae9bde9f20fcab4cd322e8a721b19a30f33
austins <austins@users.noreply.github.com>
2025-02-12 17:03:40 -0800

    Update sky color

--- app/PortalLink.tsx
+++ app/PortalLink.tsx
@@ -21,7 +21,7 @@ export function PortalLink(props: {
                 props.variant === "purple" && `bg-purple-600 hover:bg-purple-700`,
                 props.variant === "indigo" && `bg-indigo-500 hover:bg-indigo-600`,
                 props.variant === "slate" && `bg-slate-800 hover:bg-slate-900`,
-                props.variant === "sky" && `bg-sky-500 hover:bg-sky-600`,
+                props.variant === "sky" && `bg-[#0A7CC1] hover:bg-[#08639b]`,
                 props.variant === "fuchsia" && `bg-fuchsia-600 hover:bg-fuchsia-700`
             )}
         >
