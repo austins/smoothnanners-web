196a7fdc00cb027a0ab5f7910a0a0e41aa77d622
austins <1623983+austins@users.noreply.github.com>
2026-05-13 22:03:04 -0700

    Remove vite-tsconfig-paths, configure alias manually, and upgrade typescript

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    plugins: [tailwindcss(), qwikCity(), qwikVite(), tsconfigPaths({ root: "." })],
+    plugins: [tailwindcss(), qwikCity(), qwikVite()],
+    // TODO: when it's possible to upgrade to Vite 8, can be replaced with tsconfigPaths: true
+    resolve: { alias: { "~": "./src" } },
     server: {
         headers: {
             // Don't cache the server response in dev mode.
